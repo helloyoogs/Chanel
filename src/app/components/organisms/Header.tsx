76c8074ca8315454dcdb5389098490b4a2249eb9
육희영 <helloyoogs@naver.com>
2025-09-19 16:41:33 +0900

    [frontend] header 완료

--- src/app/components/organisms/Header.tsx
+++ src/app/components/organisms/Header.tsx
@@ -2,16 +2,25 @@
 
 import { headerMenu } from "@/app/constants/global.constants";
 import Link from "next/link";
+import Image from "next/image";
+import chanel_logo from "@/app/assets/img/chanel_logo.svg";
 
 export default function Header() {
   return (
-    <header>
+    <header className={"flex items-center gap-[20px] h-[50px] mx-[60px]"}>
+      <Link href={"/containers/main"}>
+        <Image
+          src={chanel_logo}
+          alt={"chanel logo"}
+          className={"w-[150px] mr-[20px]"}
+        />
+      </Link>
       {headerMenu.map((headerMenu, headerMenuIndex) => {
         return (
           <Link
             href={headerMenu.path}
             key={headerMenuIndex}
-            className={`page_item`}
+            className={"text-[#999999] text-[14px]"}
           >
             {headerMenu.name}
           </Link>
